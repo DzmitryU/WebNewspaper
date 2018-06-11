16c8c3cb35330500a3fe527d6edaf2f9d3bbeceb
dzmitryu <dulezla@gmail.com>
2018-06-11 14:38:28 +0300

    Added button for body hiding

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,12 +1,38 @@
 import React from 'react';
 
 class Article extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isOpen: true
+        }
+    }
+
+    handleButtonClick = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    };
+
     render() {
+        let body = '';
+        if (this.state.isOpen) {
+            body = (
+                <div>
+                    <div>{this.props.article.text}</div>
+                    <h3>{this.props.article.date}</h3>
+                </div>
+            );
+        }
         return (
             <div>
-                <h1>{this.props.article.title}</h1>
-                <div>{this.props.article.text}</div>
-                <h3>{this.props.article.date}</h3>
+                <h1>
+                    {this.props.article.title}
+                    <button onClick={this.handleButtonClick}>{this.state.isOpen ? 'Close' : 'Open'}</button>
+                </h1>
+                {body}
             </div>
         );
     };
