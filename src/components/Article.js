64f227762b6b2d7448c8569b1fc2fa4a5f3737b2
dzmitryu <dulezla@gmail.com>
2018-06-11 14:14:29 +0300

    Added article

---
+++ src/components/Article.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Article extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>{this.props.article.title}</h1>
+                <div>{this.props.article.text}</div>
+                <h3>{this.props.article.date}</h3>
+            </div>
+        );
+    }
+}
+
+export default Article
