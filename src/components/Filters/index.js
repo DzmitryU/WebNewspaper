6e88ae073cd890f50836a1b2b93621c57e29cbc9
dzmitryu <dulezla@gmail.com>
2018-07-05 21:56:27 +0300

    Filter articles imported from redux

--- src/components/Filters/index.js
+++ src/components/Filters/index.js
@@ -5,22 +5,38 @@ import {connect} from 'react-redux';
 import ArticleSelect from './ArticleSelect';
 import DateRangePicker from './DateRangePicker';
 
-import {articles} from '../../data/mock';
-
 class Filter extends React.Component {
     constructor(props) {
         super(props);
     }
 
     render() {
-
         return (
             <div>
                 <DateRangePicker/>
-                <ArticleSelect articles={articles}/>
+                <ArticleSelect articles={this.props.articles}/>
             </div>
         );
     }
 };
 
-export default connect()(Filter);
+Filter.propTypes = {
+    articles: PropTypes.arrayOf(
+        PropTypes.shape({
+            title: PropTypes.string.isRequired,
+            text: PropTypes.string.isRequired,
+        })
+    )
+};
+
+Filter.defaultProps = {
+    articles: []
+};
+
+const mapStateToProps = state => {
+    return {
+        articles: state.articles
+    }
+};
+
+export default connect(mapStateToProps)(Filter);
